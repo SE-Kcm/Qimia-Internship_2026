27ad4bb01aa9e2df6cd362ab9cc9607ecb062df9
Sude Koçman <sude.e.kocman@fau.de>
2026-07-06 00:05:00 +0300

    splitted into modules

---
+++ Task 6/Task.js
@@ -0,0 +1,14 @@
+export default class Task{
+    constructor(name,id){
+        this._name = name;
+        this._id = id
+    }
+
+    getName (){
+        return this._name;
+    }
+
+    getId () {
+        return this._id;
+    }
+}
